5b7b2b18fd9d030fd6d4298d2112b63b97cf4a9b
WirelessLabUSV <marian.mutescu@usm.ro>
2026-06-01 14:50:34 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.json());
 
 app.get("/", function (req, res) {
-  res.send("RAK10700 decoder running");
+  res.send("Cayenne LPP decoder running");
 });
 
 function int16(msb, lsb) {
@@ -20,58 +20,59 @@ function int24(b1, b2, b3) {
 function hexToBytes(hex) {
   var bytes = [];
   if (!hex) return bytes;
-
   hex = hex.replace(/\s+/g, "");
-
   for (var i = 0; i < hex.length; i += 2) {
     bytes.push(parseInt(hex.substr(i, 2), 16));
   }
-
   return bytes;
 }
 
-function decodeRak10700(hex) {
+function decodeCayenneLpp(hex) {
   var b = hexToBytes(hex);
   var decoded = {};
+  var analogCount = 0;
   var i = 0;
 
   while (i < b.length) {
     var ch = b[i++];
     var type = b[i++];
 
-    if ((ch === 1 && type === 0x74) || (ch === 2 && type === 0x02)) {
-      decoded.battery_voltage = int16(b[i], b[i + 1]) / 100;
-      i += 2;
-    }
+    if (type === 0x00) {
+      decoded["digital_input_" + ch] = b[i];
+      i += 1;
+    } else if (type === 0x01) {
+      decoded["digital_output_" + ch] = b[i];
+      i += 1;
+    } else if (type === 0x02) {
+      var analog = int16(b[i], b[i + 1]) / 100;
+      analogCount++;
 
-    else if (ch === 10 && type === 0x88) {
+      if (analogCount === 1) decoded.battery_voltage = analog;
+      else if (analogCount === 2) decoded.gas_resistance = analog;
+      else decoded["analog_" + ch] = analog;
+
+      i += 2;
+    } else if (type === 0x65) {
+      decoded.illuminance = ((b[i] << 8) | b[i + 1]);
+      i += 2;
+    } else if (type === 0x67) {
+      decoded.temperature = int16(b[i], b[i + 1]) / 10;
+      i += 2;
+    } else if (type === 0x68) {
+      decoded.humidity = b[i] / 2;
+      i += 1;
+    } else if (type === 0x73) {
+      decoded.pressure = ((b[i] << 8) | b[i + 1]) / 10;
+      i += 2;
+    } else if (type === 0x74) {
+      decoded.battery_voltage = ((b[i] << 8) | b[i + 1]) / 100;
+      i += 2;
+    } else if (type === 0x88) {
       decoded.latitude = int24(b[i], b[i + 1], b[i + 2]) / 10000;
       decoded.longitude = int24(b[i + 3], b[i + 4], b[i + 5]) / 10000;
       decoded.altitude = int24(b[i + 6], b[i + 7], b[i + 8]) / 100;
       i += 9;
-    }
-
-    else if ((ch === 2 || ch === 3 || ch === 6) && type === 0x68) {
-      decoded.humidity = b[i] / 2;
-      i += 1;
-    }
-
-    else if ((ch === 3 || ch === 4 || ch === 7) && type === 0x67) {
-      decoded.temperature = int16(b[i], b[i + 1]) / 10;
-      i += 2;
-    }
-
-    else if ((ch === 4 || ch === 5 || ch === 8) && type === 0x73) {
-      decoded.pressure = ((b[i] << 8) | b[i + 1]) / 10;
-      i += 2;
-    }
-
-    else if ((ch === 6 || ch === 9) && type === 0x02) {
-      decoded.gas_resistance = int16(b[i], b[i + 1]) / 100;
-      i += 2;
-    }
-
-    else {
+    } else {
       decoded.unknown_channel = ch;
       decoded.unknown_type = type;
       decoded.remaining_bytes = b.slice(i);
@@ -84,12 +85,12 @@ function decodeRak10700(hex) {
 
 app.post("/rak10700", function (req, res) {
   var payload = req.body.value && req.body.value.payload;
-  var decoded = decodeRak10700(payload);
+  var decoded = decodeCayenneLpp(payload);
 
   var response = {
-    model: "rak10700_tracker_v1",
+    model: "cayenne_lpp_v1",
     value: decoded,
-    tags: ["RAK10700", "CAYENNE_LPP"]
+    tags: ["CAYENNE_LPP"]
   };
 
   if (decoded.latitude !== undefined && decoded.longitude !== undefined) {
